359f7d01ad3433de7d7b39b11de99b6f131743e0
so_young <qksthdhd94@gmail.com>
2019-11-12 20:35:26 +0900

    가로 스크롤뷰 예제  - 가로 스크롤뷰 예제 - 페이징 처리 하고 싶다.

---
+++ app/src/main/java/com/example/a20191112_01_fragment/TestActivity.kt
@@ -0,0 +1,18 @@
+package com.example.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}

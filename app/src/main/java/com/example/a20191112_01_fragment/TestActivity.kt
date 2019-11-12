dd5a41f52056ac2c4ee26688aea01b3864d68d2c
so_young <qksthdhd94@gmail.com>
2019-11-12 20:50:58 +0900

    두개의 프래그먼트를 뷰페이징

--- app/src/main/java/com/example/a20191112_01_fragment/TestActivity.kt
+++ app/src/main/java/com/example/a20191112_01_fragment/TestActivity.kt
@@ -1,17 +1,23 @@
 package com.example.a20191112_01_fragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a20191112_01_fragment.adapters.TestViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        setupEvents()
+        setValues()
     }
     override fun setupEvents() {
     }
 
     override fun setValues() {
+        testViewPager.adapter = TestViewPagerAdapter(supportFragmentManager)
+
     }
 
 

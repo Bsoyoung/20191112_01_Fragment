ccc2dcf33ce78b0aa3e23c83c04ca64c5859e0b1
so_young <qksthdhd94@gmail.com>
2019-11-12 19:39:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191112_01_fragment/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.example.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :AppCompatActivity(){
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
